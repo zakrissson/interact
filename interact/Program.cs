e16abed903d0883311c196d2a3dab792d64acdf6
Ronja Zakrisson <zakrissonronja@gmail.com>
2021-09-15 10:36:38 +0200

    luyfkuyfkuyf,jyyjuf

--- interact/Program.cs
+++ interact/Program.cs
@@ -26,126 +26,52 @@
 
 //Console.ReadLine();
 
-string yes = "yes";
-string no = "no";
+string question;
+string question2;
+string question3;
 
-yes = yes.ToLower();
-no = no.ToLower();
-
-Console.WriteLine("[Skyrim opens with an Imperial wagon driving four prisoners down a snowy mountain pass.]");
-Console.WriteLine("(Press ENTER to continue)");
-Console.ReadLine();
-
-Console.WriteLine("In this series of text you'll control the story");
-Console.ReadLine();
-
-Console.WriteLine("Slighty.");
-Console.ReadLine();
-
-Console.WriteLine("Maybe.");
-Console.ReadLine();
-
-Console.WriteLine("[All are seated and bound; the one dressed in finery is gagged.]");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: Hey, you. You’re finally awake. You were trying to cross the border, right?");
+Console.WriteLine("You come across a fucktard. Talk to him?");
 Console.WriteLine("(Yes/No)");
-Console.ReadLine();
+question=Console.ReadLine();
 
-if (yes == "yes")
+if (question == "yes")
 {
-    Console.WriteLine("Lokir: Sucks to be you then.");
+    Console.WriteLine("He ends your life.");
+    Console.WriteLine("Must suck to be you.");
     Console.ReadLine();
 }
 
-if (no == "no")
+if (question == "no")
 {
-    Console.WriteLine("Lokir: Then what the fuck were you up to.");
+    Console.WriteLine("You walk away, but hes following you.");
+    Console.WriteLine("Confront him? (Yes/No)");
+    question2=Console.ReadLine();
+
+    if (question2 == "yes")
+    {
+      Console.WriteLine("He bashes your head.");
+      Console.WriteLine("I don't even feel remorse for you, stupid fuck.");
+      Console.ReadLine();
+    }
+
+    if (question2 == "no")
+    {
+      Console.WriteLine("He starts running. and catches up to you.");
+      Console.WriteLine("Punch? Yes/No");
+      question3=Console.ReadLine();
+  
+    if (question3 == "yes")
+    {
+      Console.WriteLine("You uppercut his chin.");
+      Console.WriteLine("Make sure to run before the cops arrive, they won't believe you.");
+      Console.ReadLine();
+    }
+
+  if (question3 == "no")
+  {
+    Console.WriteLine("He stabs your gut and you succumb to your injuries.");
+    Console.WriteLine("Too bad.");
+    Console.ReadLine();
+  }
+    }
 }
-
-else
-{
-    Console.WriteLine("Ralof:Ok then.");
-};
-
-Console.ReadLine();
-
-Console.WriteLine("Ralof: Walked right into that Imperial ambush, same as us, and that thief over there.");
-Console.ReadLine();
-
-Console.WriteLine("Lokir: Damn you Stormcloaks. Skyrim was fine until you came along. Empire was nice and lazy.");
-Console.WriteLine("If they hadn’t been looking for you, I could’ve stolen that horse and been half way to Hammerfell.");
-Console.WriteLine("You there. You and me — we shouldn't be here. It’s these Stormcloaks the Empire wants.");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: We’re all brothers and sisters in binds now, thief.");
-Console.ReadLine();
-
-Console.WriteLine("Imperial Soldier: Shut up back there!");
-Console.ReadLine();
-
-Console.WriteLine("[Lokir looks at the gagged man.]");
-Console.ReadLine();
-
-Console.WriteLine("Lokir: And what’s wrong with him?");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: Watch your tongue! You’re speaking to Ulfric Stormcloak, the true High King.");
-Console.ReadLine();
-
-Console.WriteLine("Lokir: Ulfric? The Jarl of Windhelm? You’re the leader of the rebellion. But if they captured you…");
-Console.WriteLine("Oh gods, where are they taking us?");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: I don’t know where we’re going, but Sovngarde awaits.");
-Console.ReadLine();
-
-Console.WriteLine("Lokir: No, this can’t be happening. This isn’t happening.");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: Hey, what village are you from, horse thief?");
-Console.ReadLine();
-
-Console.WriteLine("Lokir: Why do you care?");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: A Nord’s last thoughts should be of home.");
-Console.ReadLine();
-
-Console.WriteLine("Lokir: Rorikstead. I’m… I’m from Rorikstead.");
-Console.ReadLine();
-
-Console.WriteLine("[They approach the village of Helgen. A soldier calls out to the lead wagon.]");
-Console.ReadLine();
-
-Console.WriteLine("Imperial Soldier: General Tullius, sir! The headsman is waiting!");
-Console.ReadLine();
-
-Console.WriteLine("General Tullius: Good. Let’s get this over with.");
-Console.ReadLine();
-
-Console.WriteLine("Lokir: Shor, Mara, Dibella, Kynareth, Akatosh. Divines, please help me.");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: Look at him, General Tullius the Military Governor. And it looks like the Thalmor are with him.");
-Console.WriteLine("D**n elves. I bet they had something to do with this.");
-Console.ReadLine();
-
-Console.WriteLine("Ralof: This is Helgen. I used to be sweet on a girl from here. Wonder if Vilod is still making that mead with juniper berries mixed in.");
-Console.WriteLine("Funny…when I was a boy, Imperial walls and towers used to make me feel so safe.");
-Console.ReadLine();
-
-Console.WriteLine("[A man and son watch the prisoners pull into town.]");
-Console.ReadLine();
-
-Console.WriteLine("Haming: Who are they, daddy? Where are they going?");
-Console.ReadLine();
-
-Console.WriteLine("Torolf: You need to go inside, little cub.");
-Console.ReadLine();
-
-Console.WriteLine("Haming: Why? I want to watch the soldiers.");
-Console.ReadLine();
-
-Console.WriteLine("Torolf: Inside the house. Now.");
-Console.ReadLine(); 
